7dfe0d9742fe1731d44687e96c0ddb5072a72de6
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2018-05-11 19:44:06 -0400

    first commit

---
+++ Assets/View2DTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View2DTest : MonoBehaviour {
+	public List<GameObject> prefabs;
+	public View2dMain _view2D;
+	public List<View2dMain.ItemIndex> movableItems = new List<View2dMain.ItemIndex>(); 
+
+	private int _size = 20;
+
+
+	// Use this for initialization
+	void Start () {
+		if (_view2D == null)
+			return;
+		Dictionary<string, GameObject> name2Object = new Dictionary<string,GameObject>();
+		foreach (GameObject obj in prefabs) {
+			name2Object.Add (obj.name, obj);
+		}
+		_view2D.LoadAssets (name2Object);
+		_view2D.DescribeEntireMap (BuildMap());
+
+		AddMovingStuff (2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.value < .5*Time.deltaTime)
+			AddMovingStuff (2);
+		IssueMovements ();
+	}
+
+	private string[,] BuildMap(){
+		var map = new string[_size,_size];
+		for (int i = 0; i < map.GetLength (0); ++i) { 
+			for (int j = 0; j < map.GetLength (1); ++j) { 
+				if (Random.value < .8) {
+					map [i, j] = "DirtTile";
+				} else {
+					map [i, j] = "WaterTile";
+				}
+			}	
+		}
+		return map;
+	}
+
+	private int GetRandomPos() {
+		return Mathf.FloorToInt (Random.value * _size);
+	}
+
+	private void AddMovingStuff(int count){
+		for (int i = 0; i < count; i++) {
+			movableItems.Add(_view2D.CreateItem ("person", GetRandomPos(), GetRandomPos(), -1));
+		}
+	}
+
+	private void IssueMovements(){
+		foreach (var index in movableItems) {
+			if (Random.value < Time.deltaTime * .3) {
+				_view2D.InterpolateToPosition(index, GetRandomPos (), GetRandomPos (), 1f);
+			}
+		}
+	}
+}
---
+++ Assets/View2dMain.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View2dMain : MonoBehaviour {
+	private Dictionary<string, VisualRepresentation> _assetDictionary = new Dictionary<string, VisualRepresentation>();
+	private TileMap map;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void LoadAssets(Dictionary<string, GameObject> assets){
+		foreach (string name in assets.Keys) {
+			_assetDictionary [name] = new VisualRepresentation (assets [name]);
+			//yield return null; //slightly annoying to use
+		}
+		//yield break;
+	}
+
+	// This replaces the existing map.
+	public void DescribeEntireMap(string[,] groundTiles) {
+		
+		map = new TileMap (groundTiles.GetLength(0), groundTiles.GetLength(1));
+		for (int i = 0; i < groundTiles.GetLength (0); i++) {
+			for (int j = 0; j < groundTiles.GetLength (1); j++) {
+				InstantiateObject (groundTiles [i, j], i, j, 0);
+			}
+		}
+	}
+
+	public void InterpolateToPosition(ItemIndex index, int x, int y, float duration){
+		StartCoroutine(InterpolationCoroutine(index, x, y, duration));
+	}
+
+	public void MoveItem(ItemIndex index, int x, int y){
+		RemoveItem (index);
+		AddItem (index, x, y);
+	}
+
+	private IEnumerator InterpolationCoroutine(ItemIndex index, int endX, int endY, float duration){
+		float elapsedTime=0;
+		int startX = index._x;
+		int startY = index._y;
+		while(elapsedTime<duration){
+			elapsedTime = Mathf.Min (elapsedTime + Time.deltaTime, duration);
+			float fraction = elapsedTime / duration;
+			float x = (1 - fraction) * startX + fraction * endX;
+			float y = (1 - fraction) * startY + fraction * endY;
+			MoveItem (index, Mathf.FloorToInt(x), Mathf.FloorToInt(y));
+			index._item.transform.position = new Vector3 (x, y, index._item.transform.position.z);
+			yield return null;
+		}
+		yield break;
+	}
+
+	public ItemIndex CreateItem(string name, int x, int y, float z){
+		ItemIndex index = InstantiateObject (name, x, y, z);
+		return AddItem (index, x, y);
+	}
+
+	private ItemIndex AddItem(ItemIndex index, int x, int y){
+		index._x = x;
+		index._y = y;
+		map.get (index._x, index._y).Add(index._item);
+		index._item.transform.position = new Vector3 (x, y, index._item.transform.position.z);
+		return index;
+	}
+
+
+
+	private void RemoveItem(ItemIndex index){
+		map.get (index._x, index._y).Remove(index._item);
+	}
+		
+	private ItemIndex InstantiateObject(string asset, int x, int y, float z){
+		var newThing = GameObject.Instantiate (_assetDictionary [asset].representation, new Vector3(x,y,z), Quaternion.identity);
+		map.add (x, y, newThing);
+		return new ItemIndex (newThing);
+	}
+
+	private class TileMap{
+		List<GameObject>[,] mapArray;
+
+		public TileMap(int xSize, int ySize){
+			mapArray=new List<GameObject>[xSize,ySize];
+		}
+
+		public List<GameObject> get(int x, int y){
+			if (x < 0 || x >= mapArray.GetLength (0) || y < 0 || y >= mapArray.GetLength (1))
+				return null;
+			return mapArray [x,y];
+		}
+
+		public bool add(int x, int y, GameObject item){
+			if (x < 0 || x >= mapArray.GetLength (0) || y < 0 || y >= mapArray.GetLength (1))
+				return false;
+			List<GameObject> objects = mapArray [x,y];
+			if (objects == null) {
+				objects = new List<GameObject> ();
+				mapArray [x,y] = objects;
+			}
+			objects.Add (item);
+			//TODO move the game object on screen to the appropriate location
+			//TODO position the item by sorting
+			return true;
+		}
+
+	}
+		
+
+	private class VisualRepresentation {
+		public GameObject representation;
+
+		public VisualRepresentation(GameObject s) {
+			representation = s;
+		}
+	}
+
+	//Index fields shouldn't be accessible to outside classes
+	public class ItemIndex{
+		public GameObject _item;
+		public int _x;
+		public int _y;
+		public ItemIndex(GameObject item){
+			_item=item;
+		}
+	}
+}
